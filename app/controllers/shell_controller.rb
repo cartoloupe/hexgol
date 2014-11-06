870c439604f3a5296f68f249f934f53d2706a64a
Vasanth Pappu <vasanth.pappu@gmail.com>
2014-11-05 20:01:49 -0500

    use session to store data between actions

--- app/controllers/shell_controller.rb
+++ app/controllers/shell_controller.rb
@@ -1,3 +1,4 @@
+require_relative '../../hex'
 class ShellController < ApplicationController
 
   def shell
@@ -7,9 +8,30 @@ class ShellController < ApplicationController
       round = Round.create points: "[[0,1],[60,4],[240,1],[0,0]]"
     end
 
+    session[:step] ||= 0
+    session[:step] += 1
 
-    @board = [[0,1],[60,4]]
-    @display = @board
+    session[:coordinates] = [
+      [0,1,60,2],
+      [0,1,60,0],
+      [0,1,60,-1],
+      [0,0,60,2],
+      [0,0,60,0],
+      [0,0,60,-1],
+      [0,2,60,2],
+      [0,0,60,1],
+      [0,3,60,2],
+    ]
+
+    @board = Board.new
+    (session[:coordinates]).each do |coordinate|
+      @board.add_cell Coordinate.new(coordinate)
+    end
+    session[:temp] = @board.cells.to_a
+    @board.next_cycle
+
+    session[:coordinates] = @board.cells.to_a
+    binding.pry
   end
 
 end
